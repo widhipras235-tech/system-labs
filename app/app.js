f20a160f6a7a13b455b24a3e1d1fec0c78baad17
widhipras235-tech <widhipras235@gmail.com>
2026-04-16 14:09:41 +0700

    Update app.js

--- app/app.js
+++ app/app.js
@@ -886,8 +886,8 @@ function render(data) {
         Berlaku: ${formatTanggal(mulai)} - ${formatTanggal(akhir)}  
       </div>  
 
-      <div><b>Acara:</b> ${item.acara || item.raw?.acara || "-"}</div>  
-      <div><b>Sumber:</b> ${getFileName(item.source)}</div>  
+      <div><b>🗒️Acara:</b> ${item.acara || item.raw?.acara || "-"}</div>  
+      <div><b>📑Sumber:</b> ${getFileName(item.source)}</div>  
     `  
 
     resultEl.appendChild(el)  
